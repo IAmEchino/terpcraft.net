7b11a03f9537959e576fb0a67558af4548f4183f
Josiah Altschuler <64720141+IAmEchino@users.noreply.github.com>
2024-09-13 00:25:13 -0400

    Revert "Started conversion to Django"
    This reverts commit ae4f81adc77855540e47c5affe80ae7292d0d28d.

--- terpcraft/terpcraft/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-
-def homepage(request):
-    return HttpResponse("Hello World! Welcome to the TerpCraft Homepage.")
-
-def about(request):
-    return HttpResponse("Hello world! This is the TerpCraft About page.")
